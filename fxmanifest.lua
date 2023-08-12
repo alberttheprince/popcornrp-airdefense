b118131dcc4d64b5a9d0aa27962cb90b1f7a465c
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2023-08-12 06:38:19 -0400

    Update and rename fxmanifest to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,9 @@ fx_version "cerulean"
 game "gta5"
 version "1.0"
 author "PrinceAlbert with help from Squirrel and Brian"
-descripton "a simple air defense script for prison and military base"
+descripton "a simple air defense script for prison and military base."
 
 
 client_scripts {
-    "client/client.lua",
+    "client.lua",
 }
