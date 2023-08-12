f7583f21415a7e2841d328809b32949684e606d1
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2023-08-12 06:53:23 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version "cerulean"
 game "gta5"
 version "1.0"
-author "PrinceAlbert with help from Squirrel and Brian"
+author "PrinceAlbert with help from Squirrel, Brian, Jorn, and Stan"
 descripton "a simple air defense script for prison and military base."
 
 
